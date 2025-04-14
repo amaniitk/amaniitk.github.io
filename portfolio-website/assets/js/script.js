eb2020e4eeb4049e0b7cfb92b025cd405deb301c
Aman Kumar <109408666+amaniitk@users.noreply.github.com>
2025-04-14 10:27:06 +0530

    Update script.js

--- portfolio-website/assets/js/script.js
+++ portfolio-website/assets/js/script.js
@@ -1,12 +1,21 @@
-const themeToggle = document.getElementById('theme-toggle');
-const body = document.body;
+document.addEventListener("DOMContentLoaded", () => {
+  const themeToggle = document.getElementById("theme-toggle");
+  const themeIcon = document.getElementById("theme-icon");
 
-// Check local storage to maintain user's theme preference
-if(localStorage.getItem('theme') === 'dark') {
-    body.classList.add('dark-theme');
-}
+  // Check saved theme preference in localStorage
+  if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-theme");
+    themeIcon.src = "assets/images/moon.svg";
+  }
 
-themeToggle.addEventListener('click', () => {
-    body.classList.toggle('dark-theme');
-    localStorage.setItem('theme', body.classList.contains('dark-theme') ? 'dark' : 'light');
+  themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+      themeIcon.src = "assets/images/moon.svg";
+      localStorage.setItem("theme", "dark");
+    } else {
+      themeIcon.src = "assets/images/sun.svg";
+      localStorage.setItem("theme", "light");
+    }
+  });
 });
